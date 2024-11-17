085723fb924e175ed7af4281cdf2e2ce9a48135a
clauloro <114728388+clauloro@users.noreply.github.com>
2024-11-17 23:44:22 +0100

    creación de templates

--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('about/', views.about, name="about"),
     path('contact/', views.contact, name="contact"),
+    path('portfolio/', views.portfolio, name="portfolio"),
 
 ]
