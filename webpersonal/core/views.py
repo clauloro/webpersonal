4a768d95014cdc308a42ec5b75a1f5ea042fc51e
clauloro <114728388+clauloro@users.noreply.github.com>
2024-11-17 23:32:39 +0100

    creación de paginas

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -7,7 +7,8 @@ html_base = """
  <ul>
  <li><a href="/">Portada</a></li>
  <li><a href="/about/">Acerca de</a></li>
- </ul>
+ <li><a href="/contact/">Contacto</a></li>
+ </ul>
 """
 def home(request):
  return HttpResponse(html_base + """
@@ -19,6 +20,17 @@ def about(request):
  <h2>Acerca de</h2>
  <p>Me llamo Héctor y me encanta Django!</p>
  """)
+
+def contact(request):
+    return HttpResponse(html_base + """
+    <h2>Contacto</h2>
+    <p>Aquí os dejo mi email y mis redes sociales:</p>
+    <ul>
+    <li><a href="mailto:hola@hektorprofe.net">Email</a></li>
+    <li><a href="https://github.com/hcosta">Github</a></li>
+    <li><a href="https://youtube.com">Youtube</a></li>
+     </ul>
+     """)
 
 
 
--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
     path('about/', views.about, name="about"),
+    path('contact/', views.contact, name="contact"),
 
 ]
