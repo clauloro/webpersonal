828ad9840af35f67a787882292a1fc5b23e85788
clauloro <114728388+clauloro@users.noreply.github.com>
2024-11-24 17:48:30 +0100

    correccion url

--- webpersonal/core/admin.py
+++ webpersonal/core/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
-from .models import Project
-class ProjectAdmin(admin.ModelAdmin):
- readonly_fields = ('created', 'updated')
-admin.site.register(Project, ProjectAdmin)
+
 
 
--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -2,14 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-html_base = """
- <h1>Mi Web Personal</h1>
- <ul>
- <li><a href="/">Portada</a></li>
- <li><a href="/about/">Acerca de</a></li>
- <li><a href="/contact/">Contacto</a></li>
- </ul>
-"""
+
 def home(request):
  return render(request, "core/home.html")
 
